3503261179b4dbb54fb6eca5e1801e06667c9bae
zzcQAQ <lmxdz163@163.com>
2026-04-29 16:06:56 +0800

    1

--- Skybound Odyssey/scripts/ui/slots_pannel.gd
+++ Skybound Odyssey/scripts/ui/slots_pannel.gd
@@ -4,7 +4,18 @@ extends NinePatchRect
 
 @onready var HaveBuildingsContainer = $MarginContainer/HaveBuildingsContainer
 @onready var NoBuildingsContainer = $MarginContainer/NoBuildingsContainer
+@onready var BuildingPicker = $BuildingPicker
 
 func _ready() -> void:
+	
+	BuildingPicker.visible = false
+	Global.deselected.connect(_on_deselected)
+	
 	if have_buildings == false:
 		HaveBuildingsContainer.visible = false
+
+func _on_pressed() -> void:
+	BuildingPicker.visible = true
+
+func _on_deselected():
+	BuildingPicker.visible = false
