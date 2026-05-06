5262b16c8cd183ef95236d1d41009a157576374f
zzcQAQ <lmxdz163@163.com>
2026-05-06 17:58:17 +0800

    2

--- Skybound Odyssey/scripts/buildings/base_building.gd
+++ Skybound Odyssey/scripts/buildings/base_building.gd
@@ -4,3 +4,17 @@ extends Node
 var building_cost: float
 var level: int = 0
 var build_time: float
+var sprite: String
+var sprite_node: Sprite2D
+
+func _ready():
+	load_sprite(sprite)
+
+func load_sprite(s: String) -> Texture2D:
+	var folder = "res://accests/sprites/buildings/"
+	var path = folder + s + ".png"
+	if FileAccess.file_exists(path):
+		return load(path)
+	else:
+		# 找不到贴图用默认图
+		return load(folder + "default.png")
