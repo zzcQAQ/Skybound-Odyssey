6a387fb0bfd782f26384cd21ade69e5753a7f13f
zzcQAQ <lmxdz163@163.com>
2026-05-19 09:30:15 +0800

    1

--- Skybound Odyssey/scripts/buildings/buildings.gd
+++ Skybound Odyssey/scripts/buildings/buildings.gd
@@ -8,7 +8,7 @@ static var ALL := {
 		"display_name": "Thermal Power Plant",
 		"category": "power",
 		"description": "Consume little resources to provide energy. Will slightly pollute the environment.",
-		"cost": 100,
+		"cost": 50,
 		"consume": 1,
 		"power": 50
 	},
@@ -17,7 +17,7 @@ static var ALL := {
 		"display_name": "Wind Farm",
 		"category": "power",
 		"description": "Provides a small amount of power without polluting the environment.",
-		"cost": 50,
+		"cost": 30,
 		"consume": 0,
 		"power": 10
 	},
@@ -26,7 +26,7 @@ static var ALL := {
 		"display_name": "Solar Farm",
 		"category": "power",
 		"description": "Generates a small amount of power. Output depends on stellar brightness and distance.",
-		"cost": 70,
+		"cost": 50,
 		"consume": 0,
 		"power": 10
 	},
@@ -35,7 +35,7 @@ static var ALL := {
 		"display_name": "Small Mine",
 		"category": "miner",
 		"description": "Consume a small amount of energy to obtain planetary resources.",
-		"cost": 50,
+		"cost": 30,
 		"consume": 10,
 		"Production": 3
 	},
@@ -43,10 +43,11 @@ static var ALL := {
 	"researchLab": {
 		"display_name": "Research Lab",
 		"category": "strategic",
-		"description": "Consume a small amount of energy to obtain planetary resources.",
-		"cost": 50,
+		"description": "Uses power and resources to accelerate technological research.",
+		"cost": 120,
 		"consume": 10,
-		"Production": 3
+		"power": 30,
+		"researchSpeed": 10
 	}
 }
 
